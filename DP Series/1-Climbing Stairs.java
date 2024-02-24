8a3d920eec04d9e4cde498e1b015b09541a926bf
Katroth Anil <158032470+ani2727@users.noreply.github.com>
2024-02-24 10:00:49 +0530

    Add files via upload

---
+++ DP Series/1-Climbing Stairs.java
@@ -0,0 +1,43 @@
+
+//With Space
+class Solution 
+{
+    public int climbStairs(int n) 
+    {
+        
+        if(n == 0 || n == 1) return 1;
+        
+        int dp[] = new int[n+1];
+        
+        dp[0] = 1;
+        dp[1] = 1;
+        
+        for(int i = 2; i<=n; i++) 
+        {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        
+        return dp[n];
+    }
+}
+
+//Without space
+
+class Solution 
+{
+    public int climbStairs(int n) 
+    {
+        
+        int prev2 = 1;
+        int prev = 2;
+        
+        for(int i = 2; i < n; i ++) 
+        {
+            int ans = prev+prev2;
+            prev2 = prev;
+            prev = ans;
+        }
+        return prev;
+    }
+}
+
